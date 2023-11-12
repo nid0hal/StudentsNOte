ef39ef64281159581e13d9057a1bc5ec14e6113c
Nidhal <87997974+nid0hal@users.noreply.github.com>
2023-11-12 16:05:08 +0100

    New JS cuonfig 12-11-2023
    I made new update on the Js files

--- js/min.js
+++ js/min.js
@@ -1,3 +1,4 @@
+// Recuperation les valeur des champsde formaulaire 
 const fullName = document.querySelector('.fullName');
 const dateOfBirth = document.querySelector('.dateOfBirth');
 const gender = document.querySelector('select[name="gender"]');
@@ -6,23 +7,24 @@ const task2 = document.querySelector('.task2');
 const task3 = document.querySelector('.task3');
 
 
-
-
+// Recupérer les bouttons
 const result = document.querySelector('.result');
 const submit = document.querySelector('.submit');
 const reset = document.querySelector('.reset');
 
-function validateFullName(fullName {
-        if (!fullName) {
-            return " Full name is required.";
+//Validation Rules Full Name  
 
-        }
-        if (fullName.length < 3 | | fullName.length > 20) {
-            return " Full name must be between 3 and 20 characters long. ";
-        }
-        if (!/^[a-zA-Z ]+$/.test(fullName)) {
-            return " Full name must contain only letters and spaces."
-        }
-        return null;
+function validateFullName(fullName) {
+    if (!fullName) {
+        return " Full name is required.";
 
     }
+}
+
+
+
+
+//  JS SELECTOR : 
+//     1- By balise Name 
+//     2- By Class Name
+//     3- By IdSS
